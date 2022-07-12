6a4c3104d4735803680bbcba679c2a6c2645dd9a
Arvind Shelke <ucdonor5@gmail.com>
2022-07-12 06:42:54 +0530

    mongoURI updated

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const config = require("config");
 require("dotenv").config({ path: "../config.env" });
+const db = process.env.MONGO_URI || "mongodb+srv://devpartner:devpartner@cluster0.dqnf9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 const connectDB = async () => {
   try {
